26f344a0576f6b11cd4e385f19bd3bddb9bf315a
harshaty <harshan366@gmail.com>
2023-09-19 01:02:59 +0530

    driver version error

--- com.InetBank/src/test/java/com/inet/testCase/login1.java
+++ com.InetBank/src/test/java/com/inet/testCase/login1.java
@@ -1,12 +1,36 @@
 package com.inet.testCase;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class login1 {
 	
 	@Test
 	void test1() {
-		System.out.println("HRLLOO");
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+
+		
+	    	String baseUrl = "http://www.demo.guru99.com/V4/";
+	    	
+	    	// launch Firefox and direct it to the Base URL
+	    	driver.get(baseUrl);
+
+
+		    // Enter username
+		    driver.findElement(By.name("uid")).sendKeys("mngr1336");
+
+		    // Enter Password
+		    driver.findElement(By.name("password")).sendKeys("dAnavUq");
+		   
+		    // Click Login
+		    driver.findElement(By.name("btnLogin")).click();
 	}
 
 }
