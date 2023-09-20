48f9201fd288f76fdaadc0d8f3d9b2c8eb1fc534
harshaty <harshan366@gmail.com>
2023-09-21 00:35:35 +0530

    fix: Include missing file BaseClass.java,log4j.xml, in the previous commit
    - Added BaseClass.java,log4j.xml that was missing from the previous
    commit.
    - This amendment ensures that all necessary changes are included in the
    commit.
    
    Amends: < feat(base-class): Create base class for test setup, teardown,
    and
        logging>

---
+++ com.InetBank/src/test/java/com/inet/testCase/BaseClass.java
@@ -0,0 +1,44 @@
+package com.inet.testCase;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.LogManager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.apache.logging.log4j.Logger;
+import com.aventstack.extentreports.ExtentTest;
+import com.inet.utilities.ReadConfig;
+
+public class BaseClass {
+	
+	public static  WebDriver driver;
+	public ReadConfig config = new ReadConfig();
+	public String userName = config.getUserName();
+	public String password = config.getPassword();
+	public String BaseUrl = config.getBaseUrl();
+	
+	public ExtentTest report;
+	  public static final Logger logger = org.apache.logging.log4j.LogManager.getLogger(BaseClass.class.getName());
+	//@Parameter("browser")
+	@BeforeClass
+	public void setUp() {
+		// as per the new version it automatically set the driver
+	    driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.get(BaseUrl);
+		
+		
+	}
+	@AfterClass
+	public void TearDown() {
+		 driver.quit();
+	}
+	
+	
+	
+	
+
+}
