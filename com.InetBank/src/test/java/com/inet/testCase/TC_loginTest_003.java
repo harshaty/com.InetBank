52983e1f0aeb3559a5b8b7b07e77e86a5da4caa2
harshaty <harshan366@gmail.com>
2023-09-26 00:10:01 +0530

    "added new feature"

---
+++ com.InetBank/src/test/java/com/inet/testCase/TC_loginTest_003.java
@@ -0,0 +1,61 @@
+package com.inet.testCase;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.inet.pageObject.LoginPageObject;
+
+public class TC_loginTest_003 extends BaseClass {
+	//using dataProvider without excel 
+	@DataProvider(name ="GuruTest")
+	public Object[][] testData(){
+		Object[][] data = new Object[4][2];
+		data[0][0] = config.getUserName();
+		data[0][1] = config.getPassword();
+		data[1][0] = "invalid";
+		data[1][1] = "valid";
+		//3rd row
+		data[2][0] = "valid";
+		data[2][1] = "invalid";
+		//4th row
+		data[3][0] = "invalid";
+		data[3][1] = "invalid";
+		return data;
+		
+	}
+	@Test(dataProvider = "GuruTest")
+	public void LoginDDT(String usr, String psw) throws InterruptedException {
+		
+        
+	     LoginPageObject lp = new  LoginPageObject (driver);
+	     logger.info("entering the username");
+	     lp.setUserName(usr);
+	     logger.info("entering the password");
+	     lp.setPassword(psw);
+	     logger.info("click button");
+	     lp.clickButton();
+	     Thread.sleep(3000);
+	     if(isAleartpresent() == true) {
+	    	 driver.switchTo().alert().accept();
+	    	 driver.switchTo().defaultContent();
+	    	 Assert.assertFalse(true);
+	    	 logger.fatal("invalid user and password"+ usr );
+	     }else {
+	    	 Assert.assertTrue(true);
+	    	 lp.ClickLogout();
+	    	 Thread.sleep(3000);
+	    	 driver.switchTo().alert().accept();
+	    	 driver.switchTo().defaultContent();
+	     }
+	}
+	public boolean isAleartpresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		}catch(Exception e) {
+				return false;
+		}
+	}
+    
+}
